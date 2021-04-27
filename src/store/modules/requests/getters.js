11d3dddd777524a508b29b20876accad9817d79a
maniac23 <tukaym@gmail.com>
2021-04-27 21:35:59 +0300

    filter requests received

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,5 +1,6 @@
 export default {
-  requests(state) {
-    return state.requests;
+  requests(state, getters, rootState, rootGetters) {
+    const coachId = rootGetters.userId
+    return state.requests.filter(req => req.coachId === coachId);
   }
 };
