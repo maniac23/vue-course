ffa62c531ee78487abab2fe3d4ecb50bc2f0c104
maniac23 <tukaym@gmail.com>
2021-04-27 21:52:14 +0300

    store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,25 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coach = {
-      id: context.rootGetters.userId,
+      // id: context.rootGetters.userId,
       firstName: data.firstName,
       lastName: data.lastName,
       desription: data.description,
       hourlyRate: data.rate,
       areas: data.areas
     }
-    context.commit('registerCoach', coach);
+
+    const response = await fetch(
+      `https://vue-test-5122e-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coach)
+      }
+    );
+    if(!response.ok) {
+      console.error('error');
+    }
+    context.commit('registerCoach', {...coach, id: userId});
   }
 }
