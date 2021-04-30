2be1139c62de685fbe56aa89ae34d788dad4084c
maniac23 <tukaym@gmail.com>
2021-04-30 17:50:02 +0300

    fetch requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+  <base-dialog :show="!!error" title="An error occured" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests">
         <request-item
           v-for="request in receivedRequests"
           :key="request.id"
@@ -21,12 +25,36 @@
 import RequestItem from '../../components/requests/RequestItem.vue';
 export default {
   components: { RequestItem },
+  data() {
+    return {
+      isLoading: false,
+      error: null
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
     },
     hasRequests() {
-      return !!this.receivedRequests.length;
+      return !this.isLoading && !!this.receivedRequests.length;
+    }
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (e) {
+        this.error = e.message || 'Failed to load requests';
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    handleError() {
+      this.error = null;
     }
   }
 };
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,44 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message
     };
+    const response = await fetch(
+      `https://vue-test-5122e-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest)
+      }
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || 'Failed to send a request');
+    }
+    newRequest.id = data.name;
+    newRequest.coachId = payload.coachId;
     context.commit('addRequest', newRequest);
+  },
+
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(
+      `https://vue-test-5122e-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || 'Failed to send a get requests');
+    }
+    const requests = [];
+
+    for (const key in data) {
+      requests.push({
+        id: key,
+        coachId,
+        userEmail: data[key].userEmail,
+        message: data[key].message
+      });
+    }
+    context.commit('setRequests', requests);
   }
 };
